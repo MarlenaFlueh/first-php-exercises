6c5e4c0a459f5b09ae2d89b63521fe45764e490d
MarlenaFlueh <mf@esyoil.com>
2018-03-22 11:32:06 +0100

    add foreach with array

--- about.php
+++ about.php
@@ -10,6 +10,33 @@
       <div class="starter-template">
         <h1>About us</h1>
         <p class="lead">Welcome to the about us site.</p>
+        <?php
+
+          $ware = ["Hamburg" => 1000000, "Berlin" => 1500000, "Hannover" => 800000];
+          $ware["Aachen"] = "400000";
+
+          echo "<ul>";
+          foreach($ware AS $key => $produkt) {
+            echo "<li>{$key}: {$produkt}</li>";
+          }
+          echo "</ul>";
+
+          $gemüse = ["Paprika", "Kartoffel", "Karotte", "Gurke"];
+
+          $essen = ["gemüse" => $gemüse];
+
+          echo "<ul>";
+          foreach ($essen AS $gemüse => $items) {
+            echo "<li>{$gemüse}</li><ul>";
+            foreach ($items AS $item) {
+              echo "<li>{$item}</li>";
+            }
+            echo "</ul>";
+          }
+          echo "</ul>";
+
+        ?>
       </div>
     </div>
+
 <?php require "footer.php"; ?>
--- news.php
+++ news.php
@@ -1,15 +1,41 @@
-<?php require "header.php";
-  require "nav.php"
-  ?>
+<?php
+require "header.php";
+require "nav.php";
 
-    <br />
-    <br />
-    <br />
+$produkte = [
+  [
+    "title" => "Orange",
+    "desc" => "Orangen sind gesund."
+  ],
+  [
+    "title" => "Apfel",
+    "desc" => "Äpfel sind auch gesund."
+  ],
+  [
+    "title" => "Ananas",
+    "desc" => "Ananas sind auch gesund."
+  ]
+];
 
-    <div class="container">
-      <div class="starter-template">
-        <h1>News</h1>
-        <p class="lead">Welcome to the news site.</p>
+?>
+
+  <br />
+  <br />
+  <br />
+
+  <div class="container">
+    <div class="starter-template">
+      <h1>News</h1>
+      <p class="lead">Welcome to the news site.</p>
+      <div class="container">
+        <div class="btn-group" role="group" aria-label="...">
+
+        <?php foreach ($produkte as $key => $item) { ?>
+          <button type='button' class='btn btn-default'><?= $item["title"] ?></button>
+        <?php }; ?>
+        
+        </div>
       </div>
     </div>
+  </div>
 <?php require "footer.php"; ?>
